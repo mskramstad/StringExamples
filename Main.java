2aca8b2f82c08f22b2a50c4b53c96b8ad8db5c48
Mike <mskramst@gmail.com>
2022-02-20 07:26:30 -0800

    Created start of mixString

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main (String[] args) {
+    StringExamples proj = new StringExamples();
+    proj.start();
+
+  }
+}
---
+++ StringExamples.java
@@ -0,0 +1,13 @@
+public class StringExamples {
+  
+  public void start() {
+    System.out.println(mixString("abc", "xyz"));
+    System.out.println(mixString("lincoln", "tiger"));
+
+  }
+
+  // combines two strings into a larger string alternating charactes
+  public String mixString(String a, String b) {
+    
+  return ""; 
+}
